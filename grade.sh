daf37c04344879bc460f5897ac8dcae27fed492b
nicholas cheah <nicholasseancheah@gmail.com>
2023-02-15 12:39:26 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -2,17 +2,23 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission 
 git clone $1 student-submission
-echo 'Finished cloning'
+if [[ $? -eq 0 ]]
+then
+    echo 'Finished cloning'
+else
+    echo "Cloning failed"
+    exit
+fi
 
 cd student-submission
-if [[-f ListExamples.java]]
+if [[ -f ListExamples.java ]]
 then
 
     echo "ListExamples.java found"
     cp ../TestList.java student-submission 
     javac -cp CPATH TestList.java ListExamples.java  2>compileError.txt
 
-    if [[$? -neq 0]] #assumes javac TestList.java gives error code = 0
+    if [[ $? -neq 0 ]] #assumes javac TestList.java gives error code = 0
     then
 
         cat compileError.txt
@@ -21,21 +27,21 @@ then
 
         grep "static List<String> filter(List<String> s, StringChecker sc)" ListExamples.java > filter.txt
         test -s filter.txt
-        if [[$? -neq 0]]
+        if [[ $? -neq 0 ]]
         then
             echo "filter method not found"
             exit
         fi
         grep "static List<String> merge(List<String> list1, List<String> list2)" ListExamples > merge.txt
         test -s merge.txt
-        if [[$? -neq 0]]
+        if [[ $? -neq 0 ]]
         then
             echo "merge method not found"
             exit
         fi
 
         java -cp CPATH org.junit.runner.JUNITCore TestList 2>JUnitError.txt
-        if [[$? -neq 0]]
+        if [[ $? -neq 0 ]]
         then
 
             echo "Test(s) has/have failed"
