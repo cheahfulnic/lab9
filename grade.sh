c14626c454052e20c5ab02b98cd5372ead0e177f
nicholas cheah <nicholasseancheah@gmail.com>
2023-03-01 12:09:45 -0800

    Updated grading code

--- grade.sh
+++ grade.sh
@@ -1,6 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission 
+rm -rf student-submission *.txt *.class ListExamples.java
 git clone $1 student-submission
 if [[ $? -eq 0 ]]
 then
@@ -15,32 +15,34 @@ if [[ -f ListExamples.java ]]
 then
 
     echo "ListExamples.java found"
-    cp ../TestListExamples.java student-submission
-    javac -cp CPATH TestListExamples.java ListExamples.java  2>compileError.txt
+    cp ListExamples.java ../
+    cd ..
+    javac -cp $CPATH TestListExamples.java ListExamples.java  2>compileError.txt
 
     if [[ $? -ne 0 ]] #assumes javac TestListExamples.java gives error code = 0
     then
 
+        echo "Compile error found"
         cat compileError.txt
 
     else
 
-        grep "static List<String> filter(List<String> s, StringChecker sc)" ListExamples.java > filter.txt
-        test -s filter.txt
-        if [[ $? -ne 0 ]]
-        then
-            echo "filter method not found"
-            exit
-        fi
-        grep "static List<String> merge(List<String> list1, List<String> list2)" ListExamples > merge.txt
-        test -s merge.txt
-        if [[ $? -ne 0 ]]
-        then
-            echo "merge method not found"
-            exit
-        fi
+        #grep "static List<String> filter(List<String> s, StringChecker sc)" ListExamples.java > filter.txt
+        #test -s filter.txt
+        #if [[ $? -ne 0 ]]
+        #then
+        #    echo "filter method not found"
+        #    exit
+        #fi
+        #grep "static List<String> merge(List<String> list1, List<String> list2)" ListExamples > merge.txt
+        #test -s merge.txt
+        #if [[ $? -ne 0 ]]
+        #then
+        #    echo "merge method not found"
+        #    exit
+        #fi
 
-        java -cp CPATH org.junit.runner.JUNITCore TestListExamples 2>JUnitError.txt
+        java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 1>JUnitError.txt
         if [[ $? -ne 0 ]]
         then
 
