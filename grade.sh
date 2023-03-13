ff12fcd6627c02b8e5a69d3238df37d3232fef83
nicholas cheah <nicholasseancheah@gmail.com>
2023-03-13 10:06:32 -0700

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -4,10 +4,14 @@ rm -rf student-submission
 git clone $1 student-submission
 if [[ $? -eq 0 ]]
 then
+
     echo 'Finished cloning'
+
 else
+
     echo "Cloning failed"
     exit
+
 fi
 
 cd student-submission
@@ -17,10 +21,8 @@ then
     echo "ListExamples.java found"
     cp ../TestListExamples.java ./
     cp -r ../lib ./
-
     javac -cp $CPATH *.java  2> compileError.txt
-
-    if [[ $? -ne 0 ]] #assumes javac TestListExamples.java gives error code = 0
+    if [[ $? -ne 0 ]]
     then
 
         echo "Compile error found"
@@ -32,22 +34,52 @@ then
         test -s class.txt
         if [[ $? -ne 0 ]]
         then
+
             echo "ListExamples class not found"
             exit
+
         fi
+
         grep "static List<String> filter(List<String>" ListExamples.java > filter.txt
         test -s filter.txt
         if [[ $? -ne 0 ]]
         then
-            echo "filter method incorrect/not found"
-            exit
+
+            grep "static List<String> filter(" ListExamples.java > filter.txt
+            test -s filter.txt
+            if [[ $? -ne 0 ]]
+            then
+
+                echo "filter method not found"
+                exit
+
+            else
+
+                echo "filter method incorrect argument type"
+                exit
+
+            fi
         fi
+
         grep "static List<String> merge(List<String>" ListExamples.java > merge.txt
         test -s merge.txt
         if [[ $? -ne 0 ]]
         then
-            echo "merge method incorrect/not found"
-            exit
+
+            grep "static List<String> merge(" ListExamples.java > filter.txt
+            test -s filter.txt
+            if [[ $? -ne 0 ]]
+            then
+
+                echo "merge method not found"
+                exit
+
+            else
+
+                echo "merge method incorrect argument type"
+                exit
+
+            fi
         fi
 
         java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 1>JUnitError.txt
@@ -57,15 +89,12 @@ then
 
             TEST_COUNT=$(awk '/Tests run:/ {gsub(",", ""); print $3}' JUnitError.txt)
             FAILURE_COUNT=$(awk '/Tests run:/ {gsub(",", ""); print $5}' JUnitError.txt)
-
-
             echo "Test(s) has/have failed"
             echo $FAILURE_COUNT "of" $TEST_COUNT "tests failed."
 
         else
             echo "All tests have passed"
         fi
-        
     fi
 
 else
